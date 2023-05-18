5bb2de4e6b6e95abf3866c352f03c3ca0e902113
Evelyn Jimenez <evey.plt@gmail.com>
2023-05-18 10:34:27 -0500

    removed debugging

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,10 @@
 //User Entery Options
 var passwordLength;
-var confirmCharacter
-var confirmNumber
-var confirmUppercase
-var confirmLowercase
+var confirmCharacter;
+var confirmNumber;
+var confirmUppercase;
+var confirmLowercase;
+var userChoices;
 
 //Password Variables
 
@@ -36,25 +37,78 @@ function generatePassword() {
   } else if (passwordLength < 8 || passwordLength > 128) {
     passwordLength = parseInt(prompt("You must choose between 8 and 128"));
   } else {
-    confirmNumber = confirm("Will this contain numers?");
-    confirmCharacter = confirm("Will this contain special characters?");
-    confirmUppercase = confirm("Will this contain Uppercase letters?");
-    confirmLowercase = confirm("Will this contain Lowercase letters?");
+    confirmNumber = confirm("Will this contain numers? (OK = Yes, Cancel = No)");
+    confirmCharacter = confirm("Will this contain special characters? (OK = Yes, Cancel = No)");
+    confirmUppercase = confirm("Will this contain Uppercase letters? (OK = Yes, Cancel = No)");
+    confirmLowercase = confirm("Will this contain Lowercase letters? (OK = Yes, Cancel = No)");
   }
 
-    let charCodes = LOWERCASE_CHAR_CODES
-    if (confirmUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-    if (confirmCharacter) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
-    if (confirmNumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  let charCodes = [];
+     
+    if (confirmLowercase) charCodes = charCodes.concat(LOWERCASE_CHAR_CODES);
+    if (confirmUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    if (confirmCharacter) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
+    if (confirmNumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
 
-console.log("My charCodes: "+charCodes);
+    // When no value is selected
+    if (!confirmLowercase && !confirmUppercase && !confirmCharacter && !confirmNumber){
+      alert ("You Must Select a Value");
+      return;
+    } 
+
+    // 3 true choices  
+    else if (confirmLowercase && confirmUppercase && confirmCharacter) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(UPPERCASE_CHAR_CODES, SYMBOL_CHAR_CODES);
+    }
+    else if (confirmLowercase && confirmUppercase && confirmCharacter) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(UPPERCASE_CHAR_CODES, SYMBOL_CHAR_CODES);
+    }
+    else if (confirmLowercase && confirmNumber && confirmCharacter) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(NUMBER_CHAR_CODES, SYMBOL_CHAR_CODES);
+    }
+    else if (confirmUppercase && confirmNumber && confirmCharacter) {
+      userChoices = UPPERCASE_CHAR_CODES.concat(NUMBER_CHAR_CODES, SYMBOL_CHAR_CODES);
+    }
+
+    // 2 true choices
+    else if (confirmLowercase && confirmUppercase) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(UPPERCASE_CHAR_CODES);
+    }
+    else if (confirmLowercase && confirmNumber) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(NUMBER_CHAR_CODES);
+    }
+    else if (confirmLowercase && confirmCharacter) {
+      userChoices = LOWERCASE_CHAR_CODES.concat(SYMBOL_CHAR_CODES);
+    }
+    else if (confirmUppercase && confirmNumber) {
+      userChoices = UPPERCASE_CHAR_CODES.concat(NUMBER_CHAR_CODES);
+    }
+    else if (confirmUppercase && confirmCharacter) {
+      userChoices = UPPERCASE_CHAR_CODES.concat(SYMBOL_CHAR_CODES);
+    }
+    else if (confirmNumber && confirmCharacter) {
+      userChoices = NUMBER_CHAR_CODES.concat(SYMBOL_CHAR_CODES);
+    }
+    // 1 true choices
+    else if (confirmLowercase) {
+      userChoices = LOWERCASE_CHAR_CODES;
+    }
+    else if (confirmUppercase) {
+      userChoices = UPPERCASE_CHAR_CODES;
+    }
+    else if (confirmNumber) {
+      userChoices = NUMBER_CHAR_CODES;
+    }
+    else if (confirmCharacter) {
+      userChoices = SYMBOL_CHAR_CODES;
+    };
+
+   
     const passwordCharacters = []
     for (let i = 0; i < passwordLength; i++) {
       const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-      console.log("passwordCharacters.push -->"+ String.fromCharCode(characterCode));
       passwordCharacters.push(String.fromCharCode(characterCode))
 }
-console.log("Outputting array: "+passwordCharacters.join(""));
 document.getElementById('password').textContent = passwordCharacters.join("");
      
 }
