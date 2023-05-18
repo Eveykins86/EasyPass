23ca34b0ec0d248ef0a43b323e419e3183a83462
Evelyn Jimenez <evey.plt@gmail.com>
2023-05-18 00:55:12 -0500

    Debbuged jscode and HTML

--- Develop/script.js
+++ Develop/script.js
@@ -5,20 +5,17 @@ var confirmNumber
 var confirmUppercase
 var confirmLowercase
 
-
 //Password Variables
 
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-  arrayFromLowToHigh(58, 64)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)
 ).concat(
   arrayFromLowToHigh(91, 96)
 ).concat(
   arrayFromLowToHigh(123, 126)
 )
-var password
 
 function arrayFromLowToHigh(low, high) {
   const array = []
@@ -31,8 +28,9 @@ function arrayFromLowToHigh(low, high) {
 // Assignment code here
 
 function generatePassword() {
+
   passwordLength = parseInt(prompt("How many characters would you like your password to be? (Must be 8-128 characters in length)"));
-  
+
   if (!passwordLength) {
     alert ("You Must Enter a Number");
   } else if (passwordLength < 8 || passwordLength > 128) {
@@ -45,28 +43,22 @@ function generatePassword() {
   }
 
     let charCodes = LOWERCASE_CHAR_CODES
-    if (confirmUppercase) charCodes = charCodes.concat
-    (UPPERCASE_CHAR_CODES)
+    if (confirmUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
     if (confirmCharacter) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
     if (confirmNumber) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
 
+console.log("My charCodes: "+charCodes);
     const passwordCharacters = []
     for (let i = 0; i < passwordLength; i++) {
       const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+      console.log("passwordCharacters.push -->"+ String.fromCharCode(characterCode));
       passwordCharacters.push(String.fromCharCode(characterCode))
-    }
-    return(characterCode)
-
+}
+console.log("Outputting array: "+passwordCharacters.join(""));
+document.getElementById('password').textContent = passwordCharacters.join("");
+     
 }
 
-      //Display password
-      var passwordText = document.querySelector("#password");
-      function writePassword(characterCode) {
-        if (password.length === 0) {
-          return;
-        }
-         passwordText.value = characterCode;
-      }
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
